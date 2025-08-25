d714e4b916d0b6a1da69c48d08bdb15fa7ea2aee
Curtis Barnhart <cbarnhart@westmont.edu>
2025-08-24 18:08:10 -0700

    new routing system

---
+++ src/components/BlogPost.tsx
@@ -0,0 +1,22 @@
+import { BlogEntry } from "@/types";
+import React from "react";
+type Props = {
+    post: BlogEntry | null
+}
+
+export default function BlogPost ({ post } : Props) {
+    if (!post) return null; 
+
+    return (
+        <main className="p-6 max-w-3xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+            <div className="prose prose-neutral max-w-none">
+                {post.content.map((item, idx) => (
+                    <React.Fragment key={idx}>
+                        {item.to_jsx()}
+                    </React.Fragment>
+                ))}
+            </div>
+        </main>    
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,94 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+'use client'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { useEffect, useState } from "react";
+import BlogPost from "@/components/BlogPost";
+import { BlogEntry, BlogParagraph, BlogHeader, BlogImage, City } from "@/types";
+//import { getBlogData } from "@/pages/api/util";
+import { InferGetStaticPropsType } from "next";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+type staticProps = {
+    props: {
+        posts: any[]
+    }
+}
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+// This function gets called at build time
+export async function getStaticProps(): Promise<staticProps> {
+    // Call an external API endpoint to get posts
+
+    const city: City = {location: {x: 0, y: 0}, name: "nowhere"};
+    const example1 = new BlogEntry(city, "A Blog Entry", [
+        new BlogHeader("Header 1"),
+        new BlogParagraph("paragraph 1"),
+        new BlogParagraph("paragraph 2"),
+        new BlogParagraph("paragraph 3"),
+        new BlogHeader("Header 2"),
+        new BlogParagraph("paragraph 4"),
+        new BlogParagraph("paragraph 5"),
+    ])
+    const example2 = new BlogEntry(city, "Another Blog Entry", [
+        new BlogHeader("Header 1"),
+        new BlogParagraph("paragraph 1"),
+        new BlogParagraph("paragraph 2"),
+        new BlogParagraph("paragraph 3"),
+        new BlogHeader("Header 2"),
+        new BlogParagraph("paragraph 4"),
+        new BlogParagraph("paragraph 5"),
+    ])
+    const res = [example1.serialize(), example2.serialize()];
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            posts: res
+        },
+    }
+}
+
+// TODO: Need to fetch `posts` (by calling some API endpoint)
+//       before this page can be pre-rendered.
+export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+    const [ currentBlog, setCurrentBlog ] = useState<BlogEntry | null>(null);
+
+    const allBlogs: BlogEntry[] = [];
+    for (const e of posts) {
+        allBlogs.push(BlogEntry.deserialize(e));
+    }
+
+    useEffect(() => {
+        const L = require("leaflet");
+
+        const map = L.map('map').setView([34.44928, -119.66111], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        const marker0 = L.marker([34.44928, -119.66111]).addTo(map);
+        marker0.bindPopup("<b>Westmont College</b>");
+        marker0.on("click", () => {setCurrentBlog(allBlogs[0])});
+
+        const marker1 = L.marker([33.722108, -118.014067]).addTo(map);
+        marker1.bindPopup("<b>Levi's House</b>");
+        marker1.on("click", () => {setCurrentBlog(allBlogs[1])});
+
+        const marker2 = L.marker([32.975588, -117.136778]).addTo(map);
+        marker2.bindPopup("<b>Curtis' House</b");
+        marker2.on("click", () => {setCurrentBlog(allBlogs[2])});
+
+        map.on("click", () => setCurrentBlog(null));
+    }, [])
+
+    return (
+        <>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+                integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+                crossOrigin=""/>
+            <ul>
+                <div id="map" className="h-120 w-full"></div>
+            </ul>
+            <BlogPost post={currentBlog}></BlogPost> 
+        </>
+    )
 }
